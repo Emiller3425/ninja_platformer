ea07580c1727107eab65c241c89616777c1e3906
Ethan Miller <milleret@mail.gvsu.edu>
2024-08-21 07:06:51 -0400

    music

--- main.py
+++ main.py
@@ -160,32 +160,32 @@ class Game:
 
     async def run(self):
         while True:
-            # self.play_music()
+            self.play_music()
             if (self.show_start_screen):
-                # self.music['beat'].stop()
+                self.music['beat'].stop()
                 self.show_start_screen_screen() 
                 self.pygame_play_mixer = True
             elif (self.show_level_selector):
-                # self.music['beat'].stop()
+                self.music['beat'].stop()
                 self.pygame_play_mixer = True
                 self.show_level_selector_screen()
             elif (self.player.dead):
-                # self.music['beat'].stop()
+                self.music['beat'].stop()
                 self.pygame_play_mixer = True
                 await self.iris_out_and_reset()
             elif self.is_paused:
-                # self.music['beat'].stop()
+                self.music['beat'].stop()
                 self.pygame_play_mixer = True
                 self.show_pause_menu()
             else:
                 self.main()
             await asyncio.sleep(0)
 
-    # def play_music(self):
-    #     if self.pygame_play_mixer:
-    #         self.pygame_play_mixer = False
-    #         self.music['beat'].set_volume(0.05)
-    #         self.music['beat'].play(-1)
+    def play_music(self):
+        if self.pygame_play_mixer:
+            self.pygame_play_mixer = False
+            self.music['beat'].set_volume(0.05)
+            self.music['beat'].play(-1)
 
        
     def show_start_screen_screen(self):
